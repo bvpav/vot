f4e564cd41bd4fb852d396ba696b94e4b2ff0690
bvpav <bvpav@tutanota.com>
2023-12-03 21:18:19 +0200

    Refactor RPCProcedure connection handling

--- docker-message-queue-service/api/rpc.py
+++ docker-message-queue-service/api/rpc.py
@@ -3,37 +3,48 @@ import uuid
 
 import pika
 
-amqp_conn = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        host='rabbitmq',
-        credentials=pika.PlainCredentials('guest', 'guest')
-    )
+params = pika.ConnectionParameters(
+    host='rabbitmq',
+    credentials=pika.PlainCredentials('guest', 'guest')
 )
 
 
 class RPCProcedure:
-    def __init__(self, connection, routing_key, serialize, deserialize):
-        self.connection = connection
-        self.channel = connection.channel()
-        self.callback_queue = self.channel.queue_declare(queue='', exclusive=True).method.queue
+    def __init__(self, connection_parameters, routing_key, serialize, deserialize):
+        self.connection_parameters = connection_parameters
+        self.connection = None
+        self.channel = None
+        self.callback_queue = None
+
         self.routing_key = routing_key
         self.serialize = serialize
         self.deserialize = deserialize
 
+        self.correlation_id = None
+        self.response = None
+
+    def connect(self):
+        self.connection = pika.BlockingConnection(self.connection_parameters)
+        self.channel = self.connection.channel()
+        self.callback_queue = self.channel.queue_declare(queue='', exclusive=True).method.queue
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
             auto_ack=True,
         )
 
-        self.correlation_id = None
-        self.response = None
+    def disconnect(self):
+        self.connection.close()
+        self.connection = None
+        self.channel = None
+        self.callback_queue = None
 
     def on_response(self, ch, method, props, body):
         if self.correlation_id == props.correlation_id:
             self.response = self.deserialize(body)
 
     def __call__(self, **kwargs):
+        self.connect()
         self.response = None
         self.correlation_id = str(uuid.uuid4())
         self.channel.basic_publish(
@@ -46,10 +57,11 @@ class RPCProcedure:
             body=self.serialize(kwargs),
         )
         self.connection.process_data_events(time_limit=None)
+        self.disconnect()
         return self.response
 
 
-create_video = RPCProcedure(amqp_conn, 'videos.create_video', serialize=json.dumps, deserialize=json.loads)
-update_video = RPCProcedure(amqp_conn, 'videos.update_video', serialize=json.dumps, deserialize=json.loads)
-get_video = RPCProcedure(amqp_conn, 'videos.get_video', serialize=json.dumps, deserialize=json.loads)
-list_videos = RPCProcedure(amqp_conn, 'videos.list_videos', serialize=json.dumps, deserialize=json.loads)
+create_video = RPCProcedure(params, 'videos.create_video', serialize=json.dumps, deserialize=json.loads)
+update_video = RPCProcedure(params, 'videos.update_video', serialize=json.dumps, deserialize=json.loads)
+get_video = RPCProcedure(params, 'videos.get_video', serialize=json.dumps, deserialize=json.loads)
+list_videos = RPCProcedure(params, 'videos.list_videos', serialize=json.dumps, deserialize=json.loads)
